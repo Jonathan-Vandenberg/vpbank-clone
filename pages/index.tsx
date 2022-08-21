d6c5dad57f19fd8891adb3470a34f561f63ca133
jon-vdb <jonvdberg8@gmail.com>
2022-08-21 19:21:35 +0700

    manifest removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,10 @@ const App: React.FC<{ temperature: string }> = ({ temperature = "28" }) => {
     <main>
       <Head>
         <title>VPBank home page</title>
-        <link
-          rel="manifest"
-          crossOrigin="use-credentials"
-          href="/manifest.json"
-        />
-        <link rel="apple-touch-icon" href="/logo-96x96.png" />
         <meta
           name="description"
           content="VPBank is a leading Vietnamese bank that provides a wide range of services and products to its customers"
         />
-        <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
       </Head>
       <HomeMain temp={temperature} />
     </main>

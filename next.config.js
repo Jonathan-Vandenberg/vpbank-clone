3db7ea2267b4c896ec21a6e454b3c4f7b3d7e1f3
jon-vdb <jonvdberg8@gmail.com>
2022-08-21 18:20:15 +0700

    SERVICE WORKERS

--- next.config.js
+++ next.config.js
@@ -52,10 +52,16 @@ module.exports = (phase, defaultConfig) => {
 };
 
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
     dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
   },
 });
 

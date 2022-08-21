1ba71746ac86e3e7a2aaa96663eb6a52ab07b243
jon-vdb <jonvdberg8@gmail.com>
2022-08-21 16:01:09 +0700

    security headers

--- next.config.js
+++ next.config.js
@@ -13,6 +13,36 @@ const nextConfig = {
   },
 };
 
+const securityHeaders = [
+  // This header controls DNS prefetching, allowing browsers to proactively perform domain name resolution on external links, images, CSS, JavaScript, and more.
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  // This header stops pages from loading when they detect reflected cross-site scripting (XSS) attacks.
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  // This can prevent against clickjacking attacks.
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+];
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
 module.exports = {
   basePath: "/vpbank-clone",
 };

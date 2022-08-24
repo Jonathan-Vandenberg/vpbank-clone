80420012128ef9cd0737462042b7a237680a738d
jon-vdb <jonvdberg8@gmail.com>
2022-08-25 00:10:28 +0700

    final touches

--- components/HomePage/TipsAndStories.tsx
+++ components/HomePage/TipsAndStories.tsx
@@ -2,21 +2,21 @@ import CardSlider from "../UI/CardSlider";
 
 const data = [
   {
-    image: "https://picsum.photos/id/10/300/300",
+    image: "https://picsum.photos/id/10/600/600",
     id: "6",
     title: "Bank with us!",
     content:
       "We are a bank that provides you with the best services in the world. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia rem quos enim fuga id fugit vel rerum porro maxime deserunt sed molestiae quisquam veritatis et, quibusdam culpa officiis ad est.",
   },
   {
-    image: "https://picsum.photos/id/38/300/300",
+    image: "https://picsum.photos/id/38/600/600",
     id: "7",
     title: "Youth Starter Plans",
     content:
       "Banks that provides youth with the best services in the world. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia rem quos enim fuga id fugit vel rerum porro maxime deserunt sed molestiae quisquam veritatis et, quibusdam culpa officiis ad est.",
   },
   {
-    image: "https://picsum.photos/id/93/300/300",
+    image: "https://picsum.photos/id/93/600/600",
     id: "8",
     title: "Here for you!",
     content:

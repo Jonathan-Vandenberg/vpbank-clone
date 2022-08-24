adbbd460760fe0c27f5c39c39c259a8ab296c882
jon-vdb <jonvdberg8@gmail.com>
2022-08-24 12:41:35 +0700

    metal prices

--- pages/card-services.tsx
+++ pages/card-services.tsx
@@ -2,14 +2,17 @@ import axios from "axios";
 import Head from "next/head";
 import CardServicesMain from "../components/CardServices/CardServicesMain";
 
-const App: React.FC<{ temperature: string }> = ({ temperature }) => {
+const App: React.FC<{ temperature: string; metalPrices: string[] }> = ({
+  temperature,
+  metalPrices,
+}) => {
   return (
     <main>
       <Head>
         <title>Card Services</title>
         <meta name="description" content="Find the latest card services" />
       </Head>
-      <CardServicesMain temp={temperature} />
+      <CardServicesMain temp={temperature} metalPrices={metalPrices} />
     </main>
   );
 };
@@ -17,7 +20,7 @@ const App: React.FC<{ temperature: string }> = ({ temperature }) => {
 export default App;
 
 export async function getStaticProps() {
-  const options = {
+  const optionsWeather = {
     method: "GET",
     url: "https://weatherbit-v1-mashape.p.rapidapi.com/current",
     params: { lon: "106.6297", lat: "10.823" },
@@ -27,10 +30,29 @@ export async function getStaticProps() {
     },
   };
 
+  const options = {
+    method: "GET",
+    url: "https://gold-price-live.p.rapidapi.com/get_metal_prices",
+    headers: {
+      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY!,
+      "X-RapidAPI-Host": "gold-price-live.p.rapidapi.com",
+    },
+  };
+
   let temperature;
+  let metalPrices;
 
   await axios
     .request(options)
+    .then(function (response: any) {
+      metalPrices = JSON.stringify(response.data);
+    })
+    .catch(function (error: any) {
+      console.error(error);
+    });
+
+  await axios
+    .request(optionsWeather)
     .then(function (response: any) {
       temperature = JSON.stringify(response.data.data[0].temp);
     })
@@ -41,6 +63,7 @@ export async function getStaticProps() {
   return {
     props: {
       temperature,
+      metalPrices,
     },
   };
 }

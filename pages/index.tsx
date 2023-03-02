903620eb95571aeb62fc16726edf67614f7d3dab
jon-vdb <jonvdberg8@gmail.com>
2023-03-02 14:37:25 +0700

    updated DB

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,9 @@ import axios from "axios";
 import Head from "next/head";
 import HomeMain from "../components/HomePage/HomeMain";
 
-const App: React.FC<{ temperature: string; metalPrices: string[] }> = ({
-  temperature,
-  metalPrices,
-}) => {
+const App: React.FC = () => {
+  const temperature = "27";
+  const metalPrices = ["43.48723874", "2123.8923948"];
   return (
     <main>
       <Head>
@@ -22,53 +21,53 @@ const App: React.FC<{ temperature: string; metalPrices: string[] }> = ({
 
 export default App;
 
-export async function getStaticProps() {
-  const optionsWeather = {
-    method: "GET",
-    url: "https://weatherbit-v1-mashape.p.rapidapi.com/current",
-    params: { lon: "106.6297", lat: "10.823" },
-    headers: {
-      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY!,
-      "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
-    },
-  };
+// export async function getStaticProps() {
+//   const optionsWeather = {
+//     method: "GET",
+//     url: "https://weatherbit-v1-mashape.p.rapidapi.com/current",
+//     params: { lon: "106.6297", lat: "10.823" },
+//     headers: {
+//       "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY!,
+//       "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
+//     },
+//   };
 
-  const options = {
-    method: "GET",
-    url: "https://gold-price-live.p.rapidapi.com/get_metal_prices",
-    headers: {
-      "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY!,
-      "X-RapidAPI-Host": "gold-price-live.p.rapidapi.com",
-    },
-  };
+//   const options = {
+//     method: "GET",
+//     url: "https://gold-price-live.p.rapidapi.com/get_metal_prices",
+//     headers: {
+//       "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY!,
+//       "X-RapidAPI-Host": "gold-price-live.p.rapidapi.com",
+//     },
+//   };
 
-  let temperature: any;
-  let metalPrices;
+//   let temperature: any;
+//   let metalPrices;
 
-  await axios
-    .request(options)
-    .then(function (response: any) {
-      metalPrices = JSON.stringify(response.data);
-    })
-    .catch(function (error: any) {
-      console.error(error);
-    });
+//   await axios
+//     .request(options)
+//     .then(function (response: any) {
+//       metalPrices = JSON.stringify(response.data);
+//     })
+//     .catch(function (error: any) {
+//       console.error(error);
+//     });
 
-  await axios
-    .request(optionsWeather)
-    .then(function (response: any) {
-      if (response.data) {
-        temperature = JSON.stringify(response.data.data[0].temp);
-      } else temperature = "27";
-    })
-    .catch(function (error: any) {
-      console.error(error);
-    });
+//   await axios
+//     .request(optionsWeather)
+//     .then(function (response: any) {
+//       if (response.data) {
+//         temperature = JSON.stringify(response.data.data[0].temp);
+//       } else temperature = "27";
+//     })
+//     .catch(function (error: any) {
+//       console.error(error);
+//     });
 
-  return {
-    props: {
-      temperature,
-      metalPrices,
-    },
-  };
-}
+//   return {
+//     props: {
+//       temperature,
+//       metalPrices,
+//     },
+//   };
+// }
--- slices/cardIdSlice.ts
+++ slices/cardIdSlice.ts
@@ -1,27 +1,33 @@
-import { createSlice, PayloadAction, TaskAbortError } from '@reduxjs/toolkit'
-import type { RootState } from '../store/store'
+import { createSlice, PayloadAction, TaskAbortError } from "@reduxjs/toolkit";
+import type { RootState } from "../store/store";
 
 interface CardIdState {
-  cardId: string[] | undefined
+  cardId: string[] | undefined;
 }
 
 export const cardIdSlice = createSlice({
-  name: 'cardId',
+  name: "cardId",
   initialState: {
     // Default cards being show on page load
-    cardId: [ "cl6yuqjs60041vo0giip2zhp5", "cl6yv8jlh0058vo0ghvqeu1hy", "cl6yvf2z80075vo0g4y9gu1k0"],
+    cardId: [
+      "cl9cbbjlu0023v40g0flkvtvf",
+      "clakvum240011n70gxcxsnvfi",
+      "clakvv4mr0028n70gyavn015m",
+    ],
   },
   reducers: {
     addCard: (state: CardIdState, action: PayloadAction<string>) => {
-      state.cardId?.includes(action.payload) ? null : state.cardId?.push(action.payload)
+      state.cardId?.includes(action.payload)
+        ? null
+        : state.cardId?.push(action.payload);
     },
     removeCard: (state, action: PayloadAction<string>) => {
-      state.cardId = state.cardId.filter(card => card !== action.payload)
-    }
+      state.cardId = state.cardId.filter((card) => card !== action.payload);
+    },
   },
-})
+});
 
-export const { addCard, removeCard } = cardIdSlice.actions
-export default cardIdSlice.reducer
+export const { addCard, removeCard } = cardIdSlice.actions;
+export default cardIdSlice.reducer;
 // Other code such as selectors can use the imported `RootState` type
-export const selectCardId = (state: RootState) => state.cardId.cardId
+export const selectCardId = (state: RootState) => state.cardId.cardId;
